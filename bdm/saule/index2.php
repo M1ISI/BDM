9bfc8d621c9ece0a8f74d93be84f8f0c479f0ab8
root <root@jeremy-MAIN.(none)>
2014-03-12 22:53:09 +0100

    Ajout d'autres scripts de tests pour la connexion FB (les autres ne sont pas concluants...)

--- bdm/saule/index2.php
+++ bdm/saule/index2.php
@@ -1,59 +1,50 @@
-<html>
-
- <body>
 <?php
- 
-    //facebook application configuration
-    $fbconfig['appid' ] = "1449580138609793";
-    $fbconfig['secret'] = "bff79031538d7fe767ea8c7aa854d0cb
-";
- 
-    try{
-        include_once ('./libs/src/facebook.php');
-    }
-    catch(Exception $o){
- 
-        print_r($o);
- 
-    }
-    $facebook = new Facebook(array(
-      'appId'  => $fbconfig['appid'],
-      'secret' => $fbconfig['secret'],
-      'cookie' => true,
-    ));
- 
-    $user       = $facebook->getUser();
-    $loginUrl   = $facebook->getLoginUrl(
-            array(
-                'scope'         => 'email'
-            )
-    );
- 
-    if ($user) {
-      try {
-        $user_profile = $facebook->api('/me');
-        $user_friends = $facebook->api('/me/friends');
-        $access_token = $facebook->getAccessToken();
-      } catch (FacebookApiException $e) {
-        d($e); 
-        $user = null;
-      }
-    }
- 
-    if (!$user) {
-        echo "<script type='text/javascript'>top.location.href = '$loginUrl';</script>";
-        exit;
-    }
- 
-    $total_friends = count($user_friends['data']);
-    echo 'Total friends: '.$total_friends.'.<br />';
-    $start = 0;
-    while ($start < $total_friends) {
-        echo $user_friends['data'][$start]['name'];
-        echo '<br />';
-        $start++;
-    }
- 
+  // Remember to copy files from the SDK's src/ directory to a
+  // directory in your application on the server, such as php-sdk/
+  require_once('libs/src/facebook.php');
+
+  $config = array(
+    'appId' => '1449580138609793',
+    'secret' => 'bff79031538d7fe767ea8c7aa854d0cb',
+    'allowSignedRequest' => false // optional but should be set to false for non-canvas apps
+  );
+
+  $facebook = new Facebook($config);
+  $user_id = $facebook->getUser();
 ?>
-</body>
+<html>
+  <head></head>
+  <body>
+
+  <?php
+    if($user_id) {
+
+      // We have a user ID, so probably a logged in user.
+      // If not, we'll get an exception, which we handle below.
+      try {
+
+        $user_profile = $facebook->api('/me','GET');
+        echo "Name: " . $user_profile['name'];
+
+      } catch(FacebookApiException $e) {
+        // If the user is logged out, you can have a 
+        // user ID even though the access token is invalid.
+        // In this case, we'll get an exception, so we'll
+        // just ask the user to login again here.
+        $login_url = $facebook->getLoginUrl(); 
+        echo 'Please <a href="' . $login_url . '">login.</a>';
+        error_log($e->getType());
+        error_log($e->getMessage());
+      }   
+    } else {
+
+      // No user, print a link for the user to login
+      $login_url = $facebook->getLoginUrl();
+      echo 'Please <a href="' . $login_url . '">login.</a>';
+
+    }
+
+  ?>
+
+  </body>
 </html>
