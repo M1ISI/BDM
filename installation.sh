dd96c2c52f350e697df4bcb2a98dab4115ae4bf8
webanck <antoine.webanck@gmail.com>
2014-03-14 10:27:26 +0100

    Adaptation du script d'installation à Debian.

--- installation.sh
+++ installation.sh
@@ -1,4 +1,14 @@
+distrib=`lsb_release -i | cut -f2`
+version=`lsb_release -r | cut -f2`
+
 echo "Installation des paquets."
+if [ "$distrib" = "Debian" ] 
+then 
+	echo "deb http://packages.dotdeb.org stable all" >> /etc/apt/sources.list.d/php-fpm.list
+	echo "deb-src http://packages.dotdeb.org stable all" >> /etc/apt/sources.list.d/php-fpm.list
+	curl http://www.dotdeb.org/dotdeb.gpg | apt-key add -
+fi
+apt-get update
 apt-get install nginx php5-fpm fcgiwrap php5-sqlite 
 
 echo "Copie des fichiers du site."
@@ -8,7 +18,13 @@ chown -R www-data:www-data /var/www
 
 echo "Configuration de nginx."
 mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default_backup
-cp ./default /etc/nginx/sites-available/
+if [ "$distrib" = "Debian" ] 
+then 
+	cp ./debian /etc/nginx/sites-available/default
+else 
+	cp ./default /etc/nginx/sites-available/
+fi
+
 
 
 echo "Redémarrage des services."
