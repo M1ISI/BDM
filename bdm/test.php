6153cd09bc0d7b475041893819bcb1e71c11b4bc
fmarcel <fmarcel@pcj308.(none)>
2014-03-18 10:51:55 +0100

    Ajout du test de la base de donnees sqlite avec stockage d'image

---
+++ bdm/test.php
@@ -0,0 +1,8 @@
+<html>
+	<body>
+	<form enctype="multipart/form-data" action="insert.php" method="post" name="changer">
+		<input name="image"  type="file">
+		<input value="Submit" type="submit">
+	</form>
+	</body>
+</html>
