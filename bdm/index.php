6ce46f107c1f4dd4f78260704715cf3d0dfdf522
maxClog <maxime.clog@gmail.com>
2014-03-20 10:54:55 +0100

    Appel dynamique sur pommier et buisson Il reste à faire un appel dynamique sur saule
    Signed-off-by: maxClog <maxime.clog@gmail.com>

--- bdm/index.php
+++ bdm/index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 <link rel="stylesheet" href="css.css" />
+<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
 </head>
 <body>
 
@@ -9,18 +10,79 @@
 <img style="text-align:top;" src="images/welcome.jpeg" />
 <object type="image/svg+xml" data="logotree.svg">
 </object>
-<p id="info"> Clique sur un arbre pour choisir ton type de recherche </p>
 </div>
 
-</body>
+<input type="text" id="recherche" />
+<select id="langue">
+	<option value="fr">Francais</option>
+	<option value="en">English</option>
+	<option value="es">Espanol</option>
+	<option value="de">Deutsch</option>
+</select>
 
-<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
-<script src="//code.jquery.com/jquery-1.9.1.js"></script>
+<div id="buisson"></div>
+<div id="pommier"></div>
+
+</body>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+<script src="buisson/buisson.js"></script>
+<script src="pommier/Pommier.js"></script>
 <script>
 $(function() {
 $( document ).tooltip();
 });
-</script>
 
+$('document').ready(function(){
+	$('#recherche').keyup(function(){
+		var buisson = $('#buisson');
+		var pommier = $('#pommier');
+		
+		var champ = $('#recherche').val(); // récupere la valeur du champ
+		if(champ == '')
+		{
+			buisson.html('');
+			pommier.html('');
+			return false;
+		}
+		
+		var lang = $('#langue').val();
+		
+		/* Appel buisson */
+		$.ajax({
+			url: "buisson/buisson.php",
+			type: "post",
+			data: {recherche: '' + champ},
+			success: function(data){
+				// data contient le html de la page "buisson.php" apres qu'elle ait reçu les données dans le post
+				buisson.html(data); // ajoute le HTML au paragraphe
+			}
+		});
+		
+		/* Appel pommier */
+		$.ajax({
+			url: "pommier/Pommier.php",
+			type: "post",
+			data: {search: '' + champ, Langage: '' + lang},
+			success: function(data){
+				// data contient le html de la page "buisson.php" apres qu'elle ait reçu les données dans le post
+				pommier.html(data); // ajoute le HTML au paragraphe
+			}
+		});
+		
+		/* Appel saule */
+		$.ajax({
+			url: "pommier/Pommier.php",
+			type: "post",
+			data: {search: '' + champ, Langage: '' + lang},
+			success: function(data){
+				// data contient le html de la page "buisson.php" apres qu'elle ait reçu les données dans le post
+				pommier.html(data); // ajoute le HTML au paragraphe
+			}
+		});
+		
+	});
+});
+
+</script>
 </html>
--- bdm/pommier/Pommier.php
+++ bdm/pommier/Pommier.php
@@ -1,151 +1,78 @@
 <html xmlns-"httm://www.w3.org/1999/xhtml">
+<body>	
+<?php
 
-	<head>
+	$search = "toto";
 
-		
-		<script src="https://www.google.com/jsapi"type="text/javascript"></script>
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	if(isset($_POST['search']))
+	{
+		$search = $_POST['search'];
+
+		$lang = $_POST['Langage'];	
+
+
+		//Remplacement des espaces par des "+"
+
+		$search = str_replace(" ","+",$search );
 
 
 
-	 
+		if($search!= ""){
 
+			//On recupere les resulats
+			$machin = file_get_contents("http://suggestqueries.google.com/complete/search?client=chrome&hl=".$lang."&q=".$search);			
 	
 
-	<form action="Pommier.php" method="post">
- 	<p>
- 		tapez votre recherche : <input name="search" type="text" id="ChampRecherche"  />
-		<input type="submit" value="Rechercher"  />
-		
-		<select name = "Langage">
-	 	 <option value="fr">Francais</option>
-	  	 <option value="en">English</option>
-	  	 <option value="es">Espanol</option>
-		 <option value="de">Deutsch</option>
+			$cmpt = 0;
+			$finished = TRUE;
 
-		</select>
+			$data = "\0";
 
-
-		
- 	</p>
- 	</form>
-
-		
-	<?php
-
-		$search = "toto";
-
-		if(isset($_POST['search']))
-		{
-			$search = $_POST['search'];
 	
-			$lang = $_POST['Langage'];	
-	
+			echo "<u>Suggestions :</u>  <br/>";
 
-			//Remplacement des espaces par des "+"
+			//Extraction des entrees sugerees
+			for($i = 0 ; $i < strlen($machin)&&$finished == TRUE ; $i++){
 
-			$search = str_replace(" ","+",$search );
+			if($cmpt%2 == 1 && $cmpt !=1){
 
+				
+				if(substr($machin , $i , 1) != "\""){
+				
+					$data .= substr($machin , $i , 1);
 
-
-			if($search!= ""){
-
-				//On recupere les resulats
-				$machin = file_get_contents("http://suggestqueries.google.com/complete/search?client=chrome&hl=".$lang."&q=".$search);			
-		
-	
-				$cmpt = 0;
-				$finished = TRUE;
-
-				$data = "\0";
-
-		
-				echo "<u>Suggestions :</u>  <br/>";
-
-				//Extraction des entrees sugerees
-				for($i = 0 ; $i < strlen($machin)&&$finished == TRUE ; $i++){
-
-				if($cmpt%2 == 1 && $cmpt !=1){
-
-					
-					if(substr($machin , $i , 1) != "\""){
-					
-						$data .= substr($machin , $i , 1);
-
-					}
-				}
-
-				if(substr($machin , $i , 1)== "]"){
-
-					$finished = FALSE;
-				}
-
-				if(substr($machin, $i , 1)== "\""){
-
-					$cmpt++;
-
-					if($cmpt%2 == 0 && $cmpt >2){
-
-						echo ($cmpt/2 - 1) . ': <span class="resultat"> ' . $data . '</span><br />';
-						$data = "";
-
-					}	
 				}
 			}
-		}
-		else{
-			echo "recherche vide";
+
+			if(substr($machin , $i , 1)== "]"){
+
+				$finished = FALSE;
+			}
+
+			if(substr($machin, $i , 1)== "\""){
+
+				$cmpt++;
+
+				if($cmpt%2 == 0 && $cmpt >2){
+
+					echo ($cmpt/2 - 1) . ': <span class="resultat"> ' . $data . '</span><br />';
+					$data = "";
+
+				}	
+			}
 		}
 	}
-	else
+	else{
 		echo "recherche vide";
-	
-
-	?>
-
-
-	<script language="Javascript" type="text/javascript">
-		 //<!
-		google.load('search', '1',{"language" : '<?=$_POST['Langage']?>'});
-
-		function OnLoad() {
-		// Create a search control
-			var searchControl = new google.search.SearchControl();
-
-		     // Add in a full set of searchers
-			var localSearch = new google.search.LocalSearch();
-			searchControl.addSearcher(localSearch);
-			searchControl.addSearcher(new google.search.WebSearch());
-			// Set the Local Search center point
-			localSearch.setCenterPoint("New York, NY");                                                                                             
-			// tell the searcher to draw itself and tell it where to attach
-			searchControl.draw(document.getElementById("searchcontrol"));
-			// execute an inital search
-			searchControl.execute('<?=$_POST['search']?>');
-		}
-		google.setOnLoadCallback(OnLoad);
-	</script>	
-
-	<script>
+	}
+}
+else
+	echo "recherche vide";
 
 
-	$('document').ready(function(){
+?>
 
-		    $('span.resultat').click(function(){
-
-				        var texte = $(this).text();
-
-        $('#ChampRecherche').val(texte);
-
-    });
-
-});	
-
-	</script>	
-
-	</head>
-	<body>
-		<div id="searchcontrol"></div>
-	</body>
-
+<div id="searchcontrol"></div>
+<script src="https://www.google.com/jsapi"type="text/javascript"></script>
+</body>
 </html>
