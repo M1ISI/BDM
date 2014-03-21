115c00e27e75b79821529d030be64fdcea044794
Luis Mendoza PC-J4 <luisjomen2a@gmail.com>
2014-03-21 15:37:52 +0100

    Fin d'algorithe Histograme

--- bdm/couleurs/Histogram.java
+++ bdm/couleurs/Histogram.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.awt.Color;
+import java.lang.String;
 
 /*
 * Classe Histogram
@@ -46,6 +47,7 @@ public class Histogram
 		
 		return histo;
 	}
+
 	
 	public static void main(String[] args)
 	{
@@ -65,6 +67,9 @@ public class Histogram
 		Hashtable<Color,Integer> histo = getHistogram(img);
 		//System.out.println(histo);
 		
+<<<<<<< HEAD
+		Picture bande = new Picture(200, 20);
+=======
 		/*
 		* Tentative ratee d'afficher une bande de couleurs de l'image.
 		* On essayer d'afficher les couleurs dominantes de l'image en
@@ -73,14 +78,53 @@ public class Histogram
 		
 		Picture bande = new Picture(BANDE_WIDTH, BANDE_HEIGHT);
 		int curseur = 0;
+>>>>>>> e6b3287271b398d2a960b410af33de720752dc40
 		
 		// On mouline sur tous les elements de l'histogramme
 		Enumeration<Color> enumKey = histo.keys();
+
+		String couleurs= "";
+
+			int r=0,g=0,b=0,m=0,cy=0,y=0,w=0,bl=0;
+
 		while(enumKey.hasMoreElements())
 		{
 			Color c = enumKey.nextElement();
 			Integer count = histo.get(c);
 			
+<<<<<<< HEAD
+
+			int Rouge = c.getRed(), Bleu =  c.getBlue() , Vert = c.getGreen();
+
+
+
+			//bleu
+			if( (0<= Rouge)  && (127> Rouge ) && ( 0 <= Vert) && (127 > Vert) && (255>= Bleu) && (127<Bleu)){
+
+				b+=count;
+
+			}			
+			if( (0<= Rouge)  && (127> Rouge ) && ( 255 >= Vert) && (127 < Vert) && (0 <= Bleu) && (127 > Bleu)){
+
+				g+=count;
+
+			}	
+			if( (255>= Rouge)  && (0< Rouge ) && ( 0 <= Vert) && (127 > Vert) && (0 <= Bleu) && (255 > Bleu)){
+
+				r+=count;
+
+			}	
+			
+			if( (255>= Rouge)  && (127< Rouge ) && ( 0 <= Vert) && (127 > Vert) && (255>= Bleu) && (127<Bleu)){
+
+				m+=count;
+
+			}	
+			if( (0<= Rouge)  && (127> Rouge ) && ( 255 >= Vert) && (127 < Vert) && (255>= Bleu) && (127<Bleu)){
+
+				cy+=count;
+
+=======
 			/*
 			* En attendant d'avoir une bande de couleurs qui marche,
 			* on affiche les valeurs brutes de l'histogramme.
@@ -97,11 +141,86 @@ public class Histogram
 				{
 					bande.set(i, j, c);
 				}
+>>>>>>> e6b3287271b398d2a960b410af33de720752dc40
 			}
-			
-			curseur = i;
+
+
+			if( (255>= Rouge)  && (127< Rouge ) && ( 255 >= Vert) && (127 < Vert) && (0<= Bleu) && (127>Bleu)){
+
+				y+=count;
+
+			}	
+			if( (255>= Rouge)  && (127< Rouge ) && ( 255 >= Vert) && (127 < Vert) && (255 >= Bleu) && (127<Bleu)){
+
+				w+=count;
+
+			}	
+
+
+
+			if( (0<= Rouge)  && (127> Rouge ) && ( 0 <= Vert) && (127 > Vert) && (0<= Bleu) && (127>Bleu)){
+
+				bl+=count;
+
+			}
+
+
 		}
+	
 		
+<<<<<<< HEAD
+		
+		if( (b/pixelCount*100) > 20){
+
+			couleurs += " bleu comme la bite a Luis";		
+
+		}
+
+		if( (g/pixelCount*100) > 20){
+
+			couleurs += " vert ";		
+
+		}
+		if( (r/pixelCount*100) > 20){
+
+			couleurs += " rouge ";		
+
+		}
+
+		if( (m/pixelCount*100) > 20){
+
+			couleurs += " magenta ";		
+
+		}
+
+		if( (cy/pixelCount*100) > 20){
+
+			couleurs += " cyan ";		
+
+		}
+
+		if( (y/pixelCount*100) > 20){
+
+			couleurs += " jaune ";		
+
+		}
+
+		if( (w/pixelCount*100) > 20){
+
+			couleurs += " blanc ";		
+
+		}
+
+		if( (bl/pixelCount*100) > 20){
+
+			couleurs += " noir(comme franck) ";		
+
+		}
+
+		System.out.println(couleurs);
+		bande.show();
+=======
 		//bande.show(); // a decommenter pour afficher la bande
+>>>>>>> e6b3287271b398d2a960b410af33de720752dc40
 	}
 }
