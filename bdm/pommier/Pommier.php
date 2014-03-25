2af2aff8d6e248d4f8d60d337c92d397dfe9ae27
max <max@localhost.localdomain>
2014-03-25 22:08:57 +0100

    Lors du clic sur les arbres, les résultats disparaissent

--- bdm/pommier/Pommier.php
+++ bdm/pommier/Pommier.php
@@ -64,30 +64,6 @@
 else
 	echo "recherche vide";
 ?>
-<!--
-<script src="https://www.google.com/jsapi"type="text/javascript"></script>
-<script language="Javascript" type="text/javascript">
-//<!
-	google.load('search', '1',{"language" : '<?php echo $_POST['Langage'] ?>'});
-	function OnLoad() {
-		// Create a search control
-		var searchControl = new google.search.SearchControl();
-
-		// Add in a full set of searchers
-		var localSearch = new google.search.LocalSearch();
-		searchControl.addSearcher(localSearch);
-		searchControl.addSearcher(new google.search.WebSearch());
-		// Set the Local Search center point
-		localSearch.setCenterPoint("New York, NY");                                                                                             
-		// tell the searcher to draw itself and tell it where to attach
-		searchControl.draw(document.getElementById("test"));
-		// execute an inital search
-		searchControl.execute('<?php echo $_POST['search'] ?>');
-	}
-	google.setOnLoadCallback(OnLoad);
-//]]>
-</script>
--->
 <div id="test"></div>
 </body>
 </html>
