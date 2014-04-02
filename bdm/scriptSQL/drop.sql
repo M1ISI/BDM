680a5ccf4f2731ec82a8823e78d2bf76e48b172d
webanck <antoine.webanck@gmail.com>
2014-04-02 20:34:12 +0200

    Remasterisation de la base de données.

--- bdm/scriptSQL/drop.sql
+++ bdm/scriptSQL/drop.sql
@@ -1,8 +1,8 @@
-drop table COLORS;
-drop table HAVE_COLOR;
-drop table TAGS_IMAGE;
-drop table IMAGE;
-drop table KEYWORDS;
-drop table TAGS;
-drop table WORDS;
+drop table TEXTS_KEYWORDS;
 drop table TEXTS;
+drop table IMAGES;
+drop table FILES;
+drop table TYPES;
+drop table WORDS;
+drop table HAVE_COLOR;
+drop table COLORS;
