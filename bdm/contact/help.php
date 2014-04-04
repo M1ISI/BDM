8aafc443d488c83e9d0dbc80c266a7f1d8eb20b4
misao <misao@ubuntu.(none)>
2014-04-04 02:14:39 +0200

    modifications du formulaire de contact et ajout de la page d'aide

---
+++ bdm/contact/help.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+<title>Aide</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+
+<h1>Bienvenue sur Woodle !</h1>
+<p> Ceci est une page d'aide pour vous aider à mieux prendre en main notre nouveau moteur de recherche. Celui-ci offre plusieurs types de recherche pour fournir à son utilisateur un champs d'action bien plus large qu'un moteur de recherche classique.</p>
+<p> </p>
+
+
+<h2>1) Lancer une recherche </h2>
+<p> Pour faire une recherche il vous suffit d'entrer des mots-clefs dans la barre de recherche (1), ainsi que de sélectionner la langue dans laquelle vous voulez effectuer votre recherche (2). La liste des résultats s'affichera automatiquement en-dessous de la barre de recherche et elle sera classée en fonction du type de recherche correspondant (3).<br/>
+<img src="images/help1.jpg" /><p>
+
+
+<h2>2) Les différents types de recherche </h2>
+<p> Woodle propose 4 types d'approche différentes pour effectuer une recherche, chacune représentée par un type d'arbre différent. </p>
+
+<h3>a) Le pommier ou la recherche classique </h3>
+<p> La recherche classique correspond à la colonne de résultats rose. Celle-ci comporte deux parties : <br/>
+<ul> 
+<li> La première (1) correspond à une liste de mots susceptibles d'orienter votre recherche, il vous suffira de cliquer sur la recherche qui vous intéresse pour qu'elle se lance automatiquement.</li> 
+<li> La seconde (2) correspond à une liste de sites internet correspondants à votre recherche. </li> 
+</ul><br/>
+<img src="images/help2a.jpg" /></p>
+
+<h3>b) Le buisson ou la recherche exotique </h3>
+<p>La recherche exotique correspond à la colonne de résultats violette. Le but de celle-ci est d'orienter votre recherche sur un chemin moins conventionnel en vous proposant une liste de mots-clefs, plus ou moins liés à votre recherche, à les ajouter à celle-ci pour l'affiner. Pour cela un simple clic sur un mot permet de l'ajouter à la recherche déjà en cours. </p>
+
+<h3>c) Le saule pleureur ou la recherche sociale (en cours de développement)</h3>
+<p>La recherche sociale, c'est-à-dire la recherche à travers les réseaux sociaux, correspond à la colonne de résultats verte claire. Vous pouvez sélectionner les réseaux sur lequels vous voulez effectuer votre recherche. Lorsque que vous cochez une case il vous sera demandé de vous identifier sur le réseau social concerné. <br/>
+/!\ A l'heure actuelle seule la connexion à Google+ fonctionne, de plus la recherche ne renvoie pas encore de résultats. Ceci est en cours de développement.</p>
+
+<h3>d) Le sapin ou la recherche par média (en cours de développement)</h3>
+<p>La recherche par média correspond à la colonne de résultat verte foncée. Celle-ci a pour but de renvoyer une liste de médias correspondants à la recherche effectuée. <br/>
+/!\ Cette recherche n'est pas encore disponible à l'heure actuelle et en cours de développement.</p>
+
+
+<h2>3) Filtrer les résultats </h2>
+<p> Par défaut le moteur renvoie des résultats pour tous les types de recherches. Cliquer sur les arbres vous permet d'activer/désactiver l'affichage d'un type de recherche, l'arbre se grisant lorsque la recherche est innactive.<br/>
+<img src="images/help3.jpg" /></p>
+
+<h2>4) Contribuer au développement de Woodle</h2>
+<p> Si vous rencontrez un bug, avez une question, un problème d'utilisation, une suggestion, une remarque ou tout autre chose dont vous souhaitez faire part aux développeurs vous pouvez leur envoyer un message via le formulaire disponible sur la page <a href="index.php">contact</a>. </p>
+
+<h2>5) Mentions légales</h2>
+<p> <i>à venir.</i></p>
+
+<p><a href="../index.php">Retour à la page d'accueil</a></p>
+
+</body>
+</html>
