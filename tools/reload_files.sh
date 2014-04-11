f9b9d4e9726aaf3934161440746486d455eb5b77
Antoine Webanck <antoine.webanck@gmail.com>
2014-04-11 07:30:54 +0200

    Autre solution de mise-à-jour des fichiers actuels du dépôt.

--- tools/reload_files.sh
+++ tools/reload_files.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo mkdir /var/www
-sudo cp -rf ../bdm/ /var/www/
+sudo mkdir /var/www/bdm
+sudo rsync -a --exclude='.*' ../bdm/ /var/www/bdm/
 sudo chown -R www-data:www-data /var/www
