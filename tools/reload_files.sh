43c07f463e282f74be703d5899a1298809c25f2c
Antoine Webanck <antoine.webanck@gmail.com>
2014-04-20 18:50:29 +0200

    Ajout d'option pour créer les dossiers parents manquants à la création de /var/www/bdm.

--- tools/reload_files.sh
+++ tools/reload_files.sh
@@ -2,6 +2,6 @@
 
 #On se place à la racine du dépôt.
 if [ "`git rev-parse --show-cdup`" != "" ]; then cd `git rev-parse --show-cdup`; fi
-sudo mkdir /var/www/bdm
+sudo mkdir -p /var/www/bdm
 sudo rsync -a --exclude='.*' bdm/ /var/www/bdm/
 sudo chown -R www-data:www-data /var/www
