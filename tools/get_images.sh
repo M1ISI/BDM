d2decb944feccaec03645d6c9c887e0b4132205a
Franck MARCEL <franckmarcel67@gmail.com>
2014-04-30 11:49:48 +0200

    Amélioration du script de récupération d'images

--- tools/get_images.sh
+++ tools/get_images.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
+if [ "$#" -ne 1 ]; then
+	echo "Usage : sh $0 <db_name>"
+	exit 1
+fi
+
 id_type=0;
 i=0;
-echo "insert into types values ($id_type, 'jpg');" | sqlite3 test.db
+echo "insert into types values ($id_type, 'jpg');" | sqlite3 ${PWD}/$1
 while read line
 do
-	wget -P /home/fmarcel/Images/test/img $line;
+	mkdir -p ${PWD}/Images
+	wget -P ${PWD}/Images $line
 	file_name= echo $line | cut -d '/' -f10;
-	echo "insert into files (type, path, url) values ($id_type, '/home/fmarcel/Images/test/img/$file_name','test image');" | sqlite3 test.db
+	echo "insert into files (type, path, url) values ($id_type, '${PWD}/Images/$file_name','test image');" | sqlite3 ${PWD}/$1
 	echo "insert into images (file) values ($i);" | sqlite3 test.db
 	i=$i+1;
-done < urls
+done < $(dirname $0)/urls
