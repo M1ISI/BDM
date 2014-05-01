2a0ba2bcf8a08b0a1e8917767ba40206bff15125
Antoine Webanck <antoine.webanck@gmail.com>
2014-05-01 16:37:42 +0200

    Ajout du redémarrage du serveur web au script de configuration des erreurs PHP.

--- tools/set_php_hide_errors.sh
+++ tools/set_php_hide_errors.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 #Ce script modifie la configuration de PHP pour ne plus afficher les erreurs.
+php_conf='/etc/php5/fpm/php.ini'
 
-sudo sed -i 's/^error_reporting = [^\n]*$/error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT/' /etc/php5/fpm/php.ini
-sudo sed -i 's/^display_errors = [^\n]*$/error_reporting = Off/' /etc/php5/fpm/php.ini
+sudo sed -i 's/^error_reporting = [^\n]*$/error_reporting = E_ALL \& ~E_DEPRECATED \& ~E_STRICT/' $php_conf
+sudo sed -i 's/^display_errors = [^\n]*$/display_errors = Off/' $php_conf
+sudo sed -i 's/^display_startup_errors = [^\n]*$/display_startup_errors = Off/' $php_conf
+
+#On se place à la racine du dépôt pour relancer le serveur web.
+if [ "`git rev-parse --show-cdup`" != "" ]; then cd `git rev-parse --show-cdup`; fi
+sh tools/reload_web_server.sh
