a79e59f49ec154adea28ac21bfcb38993b1668c8
Peiffert <peiffert@outlook.com>
2014-05-22 15:18:39 +0200

    =dossier sons

---
+++ bdm/saule/libs/google-api-php-client/test/BaseTest (Copie en conflit de Alienware 2014-04-27).php
@@ -0,0 +1,15 @@
+ 0x06142008, result - 0, running - 1, request - found
+20:46:47.5789 Queue Request for http://iir.blizzard.com:3724/submit/BNET_APP : handle - 0x06142008, index - 230, running - 0
+20:46:48.1268 OnComplete: handle - 0x06142008, result - 0, running - 1, request - found
+21:02:52.4177 Queue Request for http://eu.patch.battle.net:1119/patch : handle - 0x06142008, index - 231, running - 0
+21:02:53.2503 OnComplete: handle - 0x06142008, result - 0, running - 1, request - found
+21:02:53.2521 Queue Request for http://public-test.patch.battle.net:1119/patch : handle - 0x06142008, index - 232, running - 0
+21:02:54.2704 OnComplete: handle - 0x06142008, result - 0, running - 1, request - found
+21:35:56.2429 Queue Request for http://eu.patch.battle.net:1119/patch : handle - 0x06142008, index - 233, running - 0
+21:35:56.5063 OnComplete: handle - 0x06142008, result - 0, running - 1, request - found
+21:35:56.5087 Queue Request for http://dist.blizzard.com.edgesuite.net/d3-pod-retail/EU/20813.direct/d3-23119-137D06EF773BC6F7BB015C088AAC8568.mfil : handle - 0x06142008, index - 234, running - 0
+21:35:56.8716 OnComplete: handle - 0x06142008, result - 0, running - 1, request - found
+22:02:54.4930 Queue Request for http://eu.patch.battle.net:1119/patch : handle - 0x06142008, index - 235, running - 0
+22:02:54.7580 OnComplete: handle - 0x06142008, result - 0, running - 1, request - found
+22:02:54.7598 Queue Request for http://public-test.patch.battle.net:1119/patch : handle - 0x06142008, index - 236, running - 0
+22:02:55.
