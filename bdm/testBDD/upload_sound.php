f79d659e8cd472c50ff708808ee896e0e7378c2d
Peiffert <peiffert@outlook.com>
2014-05-22 16:45:54 +0200

    Update upload_sound.php

--- bdm/testBDD/upload_sound.php
+++ bdm/testBDD/upload_sound.php
@@ -3,12 +3,12 @@ if($_FILES)
 {
 	$dossier = 'sons';
 	$fichier = basename($_FILES['avatar']['name']);
-	echo 'name : '.$fichier;
+	echo 'Upload du fichier : '.$fichier;
 	$taille_maxi = 10000000;
 	$taille = filesize($_FILES['avatar']['tmp_name']);
 	$extensions = array('.mp3', '.MP3');
 	$extension = strrchr($_FILES['avatar']['name'], '.'); 
-	//Début des vérifications de sécurité...
+	//Dï¿½but des vï¿½rifications de sï¿½curitï¿½...
 	if(!in_array($extension, $extensions)) //Si l'extension n'est pas dans le tableau
 	{
 		 $erreur = 'Vous devez uploader un fichier de type mp3.';
@@ -21,12 +21,12 @@ if($_FILES)
 	{
 		 //On formate le nom du fichier ici...
 		 $fichier = strtr($fichier, 
-			  'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜİàáâãäåçèéêëìíîïğòóôõöùúûüıÿ', 
+			  'ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½', 
 			  'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
 		 $fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
-		 if(move_uploaded_file($_FILES['avatar']['tmp_name'], "$dossier/$fichier")) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+		 if(move_uploaded_file($_FILES['avatar']['tmp_name'], "$dossier/$fichier")) //Si la fonction renvoie TRUE, c'est que ï¿½a a fonctionnï¿½...
 		 {
-			  echo 'Upload effectué avec succès !';
+			  echo 'Upload effectuï¿½ avec succï¿½s !';
 		 }
 		 else //Sinon (la fonction renvoie FALSE).
 		 {
