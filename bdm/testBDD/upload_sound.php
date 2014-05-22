4ee7e00848cf0e8a6dc00ff5609b448d5cb178ff
Peiffert <peiffert@outlook.com>
2014-05-22 16:10:39 +0200

    =Correction bug upload son bis

--- bdm/testBDD/upload_sound.php
+++ bdm/testBDD/upload_sound.php
@@ -23,7 +23,7 @@ if($_FILES)
 			  'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜİàáâãäåçèéêëìíîïğòóôõöùúûüıÿ', 
 			  'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
 		 $fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
-		 if(move_uploaded_file($_FILES['avatar']['tmp_name'], $dossier/$fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+		 if(move_uploaded_file($_FILES['avatar']['tmp_name'], "$dossier/$fichier")) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
 		 {
 			  echo 'Upload effectué avec succès !';
 		 }
