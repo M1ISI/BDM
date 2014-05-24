32bc65f9242ab7010cbe67043142420ac0514ba9
root <root@mainfrit.(none)>
2014-05-24 16:09:35 +0200

    Cyril : Modification de l'index : les recherches de la section sapin le sont plus affichees a tord quand on efface le champ de recherche

--- bdm/testBDD/searchMusic.php
+++ bdm/testBDD/searchMusic.php
@@ -11,21 +11,23 @@
         die();
     }
 
-    // A décommenter lorsque ce sera intégré dans la page principale
-    $searchValue = $_GET['recherche'];
-
     // Exécution de la requête de recherche
     try 
     {
-        //Changement de l'encodage. Marche pas O_o ??
-        $pdo->query("PRAGMA ENCODING=\"UTF-8\"");
+        if (isset($_GET['recherche']))
+        {
+            $searchValue = $_GET['recherche'];
 
-        //Requête de recherche préparée ne tenant pas compte de la casse
-        $stmt = $pdo->prepare("SELECT m.TITLE, m.AUTHOR, m.YEAR, s.STYLE, f.URL FROM MUSICS m, STYLES s, FILES f WHERE (s.ID_STYLE = m.STYLE)  AND (m.FILE = f.ID_FILE) AND (m.AUTHOR LIKE :searchValue OR m.TITLE LIKE :searchValue OR m.YEAR LIKE :searchValue_year OR s.STYLE LIKE :searchValue) COLLATE NOCASE GROUP BY m.TITLE");
+            //Changement de l'encodage. Marche pas O_o ??
+            $pdo->query("PRAGMA ENCODING=\"UTF-8\"");
 
-        //Remplissemnt de la requête, puis exécution.
-        $stmt->execute(array('searchValue' => '%'.$searchValue.'%', 'searchValue_year' => $searchValue));
-        $result = $stmt->fetchAll();
+            //Requête de recherche préparée ne tenant pas compte de la casse
+            $stmt = $pdo->prepare("SELECT m.TITLE, m.AUTHOR, m.YEAR, s.STYLE, f.URL FROM MUSICS m, STYLES s, FILES f WHERE (s.ID_STYLE = m.STYLE)  AND (m.FILE = f.ID_FILE) AND (m.AUTHOR LIKE :searchValue OR m.TITLE LIKE :searchValue OR m.YEAR LIKE :searchValue_year OR s.STYLE LIKE :searchValue) COLLATE NOCASE GROUP BY m.TITLE");
+
+            //Remplissemnt de la requête, puis exécution.
+            $stmt->execute(array('searchValue' => '%'.$searchValue.'%', 'searchValue_year' => $searchValue));
+            $result = $stmt->fetchAll();
+        }
     } catch (Exception $e) {
         echo "Impossible d'accéder aux résultats de la requête : ".$e->getMessage();
         die();
@@ -33,9 +35,9 @@
 
     // Affichage des résultats => pour le debug
 	echo '<p>Musiques : </p><ul>';
-	foreach($result as $r)
-	{
-		echo '<li><a href="' . $r['URL'] . '">' . $r['TITLE'] . ' - ' . $r['AUTHOR'] . ' (' . $r['STYLE'] . ')</a></li>';
-	}
-	echo '</ul>';
+    foreach($result as $r)
+    {
+	    echo '<li><a href="' . $r['URL'] . '">' . $r['TITLE'] . ' - ' . $r['AUTHOR'] . ' (' . $r['STYLE'] . ')</a></li>';
+    }
+    echo '</ul>';
 ?>
